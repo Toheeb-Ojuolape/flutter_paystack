37aeca9ad8f419647d12f06901b847d20cba2389
Tobi Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2024-09-24 19:02:44 +0100

    feat: changed text size

--- lib/src/widgets/custom_dialog.dart
+++ lib/src/widgets/custom_dialog.dart
@@ -33,7 +33,7 @@ class CustomAlertDialog extends StatelessWidget {
       children.add(new Padding(
         padding: titlePadding!,
         child: new DefaultTextStyle(
-          style: context.textTheme().headline6!,
+          style: context.textTheme().bodyMedium!,
           child: new Semantics(child: title, namesRoute: true),
         ),
       ));
